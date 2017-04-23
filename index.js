814b4776284c304a899d160636f8a3dc9dedfcee
Federico Dente <dentefederico@live.com>
2017-04-23 13:21:28 +0200

    removed break button, added functionalities to the reset button. general bugfix

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ var min = document.querySelector('#minutes-session');
 var sec = document.querySelector('#seconds-session');
 var plus = document.querySelector('#plus');
 var minus = document.querySelector('#minus');
-var chill = document.querySelector('#break-btn');
 var timerMin = document.querySelector('#minutes');
 var timerSec = document.querySelector('#seconds');
 var plusBreak = document.querySelector('#plus-break');
 var minusBreak = document.querySelector('#minus-break');
 var minuteBreak = document.querySelector('#minutes-break');
-
-
+var reset = document.querySelector('#reset-btn');
+var audio = new Audio('beep.mp3');
 var counter;
 var minutesBreak = eval(minuteBreak.innerHTML);
 var minutes = eval(min.innerHTML);
 var seconds = 60;
 
-// our pomodoro function
+// Pomodoro function
 function startPomodoro() {
+	reset.disabled = false;
 	seconds--;
 
 	if(seconds <= -1) {
@@ -29,6 +29,10 @@ function startPomodoro() {
 	if(minutes <= -1) {
 		// when the pomodoro is finished, we will start the break function
 		counter = clearInterval(counter);
+		audio.play();
+		counter = setInterval(breakTime, 1000);
+		minutes = eval(minuteBreak.innerHTML) -1;
+		seconds = 60;
 		breakTime();
 
 	}
@@ -40,27 +44,50 @@ function startPomodoro() {
 
 
 function breakTime() {
-	alert("clicked");
+	// disabling button
+	plusBreak.disabled = true;
+	minusBreak.disabled = true;
+	reset.disabled = true;
+	start.disabled = true;
+
+	seconds--;
+	if(seconds <= -1) {
+		minutes--;
+		seconds = 59;
+	}
+
+	if(minutes <= -1) {
+		counter = clearInterval(counter);
+		audio.play();
+		counter = setInterval(startPomodoro, 1000);
+		minutes = eval(min.innerHTML) - 1;
+		seconds = 60;
+		startPomodoro();
+	}
+	
+	timerMin.innerHTML = minutes;
+	timerSec.innerHTML = seconds;
+
 }
 
 
 
-////////////////////////////////
-// buttons functions
-////////////////////////////////
-
-// start function
+// start button 
 start.addEventListener("click", function() {
 	// making the buttons disabled when the start button is clicked will help avoiding bugs
 	start.disabled = true;
 	minus.disabled = true;
 	plus.disabled = true;
-
 	minutes--;
 	counter = setInterval(startPomodoro, 1000);
 	startPomodoro();
 });
 
+
+
+////////////////////////////
+// Session length buttons
+////////////////////////////
 
 
 plus.addEventListener("click", function() {
@@ -73,8 +100,8 @@ plus.addEventListener("click", function() {
 
 minus.addEventListener("click", function() {
 	// if minutes are less then 0, we set them back to 0, because otherwise we will have -1 minutes, and that's not correct
-	if(minutes <= 0) {
-		minutes = 0;
+	if(minutes <= 1) {
+		minutes = 1;
 	// if minutes are not less than 0, then we can subtract
 	} else {
 	minutes--;
@@ -86,14 +113,11 @@ minus.addEventListener("click", function() {
 });	
 
 
+
 ////////////////////////////
+// break length buttons
 ////////////////////////////
 
-chill.addEventListener("click", function() {
-	minutes = 5;
-	timerMin.innerHTML = minutes;
-	chill.disabled = true;
-});
 
 
 plusBreak.addEventListener("click", function() {
@@ -103,8 +127,8 @@ plusBreak.addEventListener("click", function() {
 
 
 minusBreak.addEventListener("click", function() {
-	if(minutesBreak <= 0) {
-		minusBreak = 0;
+	if(minutesBreak <= 1) {
+		minusBreak = 1;
 	} else {
 		minutesBreak--;
 	}
@@ -114,4 +138,12 @@ minusBreak.addEventListener("click", function() {
 
 
 
-
+// Reset button
+reset.addEventListener("click", function() {
+	// when the reset button is clicked, we want to clear our interval
+	counter = clearInterval(counter);
+	counter = setInterval(startPomodoro, 1000);
+	minutes = eval(min.innerHTML) - 1;
+	seconds = 60;
+	startPomodoro();
+});
